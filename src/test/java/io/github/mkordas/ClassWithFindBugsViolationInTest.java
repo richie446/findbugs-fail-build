907c4af0cd93c201805871fb70264ee3b95653ed
Richard Padaon <richard.padaon@navy.mil>
2016-07-20 10:39:56 -0700

    Update

--- src/test/java/io/github/mkordas/ClassWithFindBugsViolationInTest.java
+++ src/test/java/io/github/mkordas/ClassWithFindBugsViolationInTest.java
@@ -3,7 +3,14 @@ package io.github.mkordas;
 public class ClassWithFindBugsViolationInTest {
 		
 		private String name;
+		
+		public void methodThrowingException() throws Exception {
+		}
 	
     private void unusedPrivateMethod() {
+    }
+		
+		public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
